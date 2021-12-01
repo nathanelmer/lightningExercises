2e83f5a118b9f89e0cdcc47f854e9dd03dd2d241
nathanelmer <nelmer12@gmail.com>
2021-12-01 09:30:35 -0600

    wrote functions

---
+++ game.js
@@ -0,0 +1,14 @@
+const printFaveGame = (game) => {
+  return `My favorite game is ${game}`;
+};
+
+const printFaveGame = (game) => { `My favorite game is ${game}`};
+
+const printFaveGame = game => `My favorite game is ${game}`;
+
+const printFaveGame = game => {
+  return "My favorite game is" + " " + game;
+};
+
+// Initialize a function named printFaveGame . The function should have a single parameter. Call the parameter game . Have the function return the combined value of game and the string as My favorite game is ${game}.
+// Using arrow function syntax there are four ways you could write that function correctly. Try to write it all four ways.
